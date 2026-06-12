2f376f62e1c831af6e8d545d97c627bc26448c56
Andrew Hudson <ahudson@gmail.com>
2026-06-12 15:17:59 +0200

    add twelve-hours attribute

--- src/time-in.ts
+++ src/time-in.ts
@@ -1,9 +1,18 @@
+interface TimeIn {
+  isTwentyFour?: boolean;
+  showDifference?: boolean;
+  hideSeconds?: boolean;
+}
+
 class TimeIn extends HTMLElement {
   #interval?: undefined | ReturnType<typeof setTimeout>;
   #shadow: ShadowRoot;
   #now?: Temporal.PlainTime;
+  // Props
   #hideSeconds?: boolean;
   #showDifference?: boolean;
+  #isTwelveHours?: boolean;
+
   #diff?: number;
   #tz?: string;
   #label?: string;
@@ -28,12 +37,19 @@ class TimeIn extends HTMLElement {
 
   #theTimeString() {
     if (!this.#now) {
-      return;
+      return "";
     }
 
     const { hour, minute, second } = this.#now;
+    const hours = this.#isTwelveHours
+      ? (hour % 12).toString()
+      : this.#padStartNumber(hour);
+    let suffix = "";
+    if (this.#isTwelveHours) {
+      suffix = hour > 12 ? "pm" : "am";
+    }
 
-    return `<span part="number">${this.#padStartNumber(hour)}</span><span part="seperator">:</span><span part="number">${this.#padStartNumber(minute)}</span>${!this.#hideSeconds ? `<span part="seperator">:</span><span part="number">${this.#padStartNumber(second)}</span>` : ""}`;
+    return `<span part="number">${hours}</span><span part="seperator">:</span><span part="number">${this.#padStartNumber(minute)}</span>${!this.#hideSeconds ? `<span part="seperator">:</span><span part="number">${this.#padStartNumber(second)}</span>` : ""}${suffix !== "" ? `<span part="suffix">${suffix}</span>` : ""}`;
   }
 
   #nanoSecondsToHours(offset: Temporal.ZonedDateTime) {
@@ -46,20 +62,42 @@ class TimeIn extends HTMLElement {
     }
 
     const computerTime = Temporal.Now.zonedDateTimeISO();
-    // const { hour, minute, second } = computerTime;
 
     const tzTime = computerTime.withTimeZone(this.#tz);
 
     const computerTimeTimeOffsetHours = this.#nanoSecondsToHours(computerTime);
     const tzTimeOffsetHours = this.#nanoSecondsToHours(tzTime);
 
-    return tzTimeOffsetHours - computerTimeTimeOffsetHours;
+    const diffHours = tzTimeOffsetHours - computerTimeTimeOffsetHours;
+
+    return diffHours > 0 ? "+" + diffHours : diffHours;
   }
 
   #printTime() {
     this.#now = Temporal.Now.plainTimeISO(this.#tz);
 
+    const timeSelector = this.shadowRoot?.querySelector("time");
+
+    if (timeSelector) {
+      timeSelector.innerHTML = this.#theTimeString()!;
+    }
+  }
+
+  async connectedCallback() {
+    if (!window.Temporal) {
+      await this.#getTemporal();
+    }
+
+    this.#tz = this.getAttribute("tz") || undefined;
+    this.#hideSeconds = this.hasAttribute("hide-seconds");
+    this.#label = this.getAttribute("label") || undefined;
+    this.#showDifference = this.hasAttribute("show-difference");
+    this.#isTwelveHours = this.hasAttribute("twelve-hours");
+
+    this.#printTime(); // first render. i.e. before the interval
+
     if (this.#showDifference) {
+      /* @ts-expect-error TODO: fixme */
       this.#diff = this.#getTimeDifferenceInHours();
     }
 
@@ -68,20 +106,6 @@ class TimeIn extends HTMLElement {
 			<time part="time">${this.#theTimeString()}</time>
 			${this.#showDifference ? `<small part="time-difference">${this.#diff}</small>` : ""}
 		`;
-  }
-
-  async connectedCallback() {
-    if (!window.Temporal) {
-      // console.log("need the polyfill");
-      await this.#getTemporal();
-    }
-
-    this.#tz = this.getAttribute("tz") || undefined;
-    this.#hideSeconds = this.hasAttribute("hide-seconds");
-    this.#label = this.getAttribute("label") || undefined;
-    this.#showDifference = this.hasAttribute("show-difference");
-
-    this.#printTime(); // first render. i.e. before the interval
 
     this.#interval = setInterval(() => this.#printTime(), 1_000); // every 1s
   }
